21a0aaf715ebabd351b385b66e4be97bb82f0756
Liam Watts <liam.watts271@gmail.com>
2023-07-15 12:53:11 +0800

    functools: implement reduce

--- functools.cc
+++ functools.cc
@@ -22,6 +22,15 @@ constexpr std::function<T3(T1)> compose(const std::function<T2(T1)> &func1, cons
         { return func2(func1(input_arg)); }};
 }
 
+template <typename ACC, typename VAL>
+constexpr ACC reduce(const std::function<ACC(VAL, ACC)> &reducer, const std::vector<VAL> &vec, const ACC &init)
+{
+    ACC result{init};
+    for (const VAL value : vec)
+        result = reducer(result, value);
+    return result;
+}
+
 // return true if any element in the vector is true
 constexpr bool any(const std::vector<bool> &bools)
 {
@@ -53,11 +62,17 @@ constexpr bool all(const std::function<bool(T)> &predicate, const std::vector<T>
     return all(map(predicate, vec));
 }
 
+template <typename T>
+constexpr T plus(const T &num1, const T &num2) { return num1 + num2; }
+
 // TODO: implement our own reduce, and redo sum using it
+// note: no type-safety for templates, unlike haskell I can see this takes any T
+// when really it needs a T that overloads (+)
 template <typename T>
 constexpr T sum(const std::vector<T> &vec)
 {
-    return std::accumulate(vec.begin(), vec.end(), T{0}, std::plus<T>());
+    return reduce(std::function<T(T, T)>{plus<T>}, vec, T{0});
+    // return std::accumulate(vec.begin(), vec.end(), T{0}, std::plus<T>());
 }
 
 // TODO: probably can do this better
