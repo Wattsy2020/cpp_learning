ab295b39c9bfdacef3e78764b22bf3766cb883dd
Liam Watts <liam.watts271@gmail.com>
2023-07-18 18:11:50 +0800

    set: add set operations

--- set.cc
+++ set.cc
@@ -36,6 +36,8 @@ public:
     // note mutable T shouldn't be hashed, so we can add a reference here
     void add(const T &item)
     {
+        if (contains(item))
+            return;
         set_values[hash(item)].push_back(item);
         all_items.push_back(item);
     }
@@ -67,6 +69,54 @@ std::ostream &operator<<(std::ostream &os, const Set<T> &set)
         os << item << " ";
     os << "}";
     return os;
+}
+
+namespace set
+{
+    template <typename T>
+    constexpr Set<T> set_union(const Set<T> &set1, const Set<T> &set2)
+    {
+        return Set<T>(itertools::chain(set1.items(), set2.items()));
+    }
+
+    template <typename T>
+    constexpr Set<T> _intersection(const std::vector<T> &smaller_items, const Set<T> &larger_set)
+    {
+        std::function<bool(T)> overlaps = {[larger_set](const T &item)
+                                           { return larger_set.contains(item); }};
+        return Set<T>(filter(overlaps, smaller_items));
+    }
+
+    template <typename T>
+    constexpr Set<T> intersection(const Set<T> &set1, const Set<T> &set2)
+    {
+        // Take the intersection by checking which items in the smaller set are also in the larger one
+        std::vector<T> items1 = set1.items();
+        std::vector<T> items2 = set2.items();
+        if (items1.size() < items2.size())
+            return _intersection(items1, set2);
+        return _intersection(items2, set1);
+    }
+
+    template <typename T>
+    constexpr Set<T> difference(const Set<T> &set_left, const Set<T> &set_right)
+    {
+        std::function<bool(T)> no_overlap{[set_right](const T &item)
+                                          { return !set_right.contains(item); }};
+        return Set<T>(filter(no_overlap, set_left.items()));
+    }
+}
+
+void test_set_add()
+{
+    Set<int> set{};
+    set.add(1);
+    assert(set.contains(1));
+
+    // test duplicates aren't added twice
+    set.add(1);
+    assert(set.contains(1));
+    assert(set.items() == std::vector<int>{1});
 }
 
 void test_set_contains()
@@ -124,12 +174,44 @@ void test_hash_conflict()
         assert(set.contains(i));
 }
 
+void test_set_union()
+{
+    Set<int> set1{1, 2, 3};
+    Set<int> set2{2, 3, 4};
+    Set<int> unioned_set{set::set_union(set1, set2)};
+    assert(unioned_set.items() == (std::vector<int>{1, 2, 3, 4}));
+}
+
+void test_set_intersection()
+{
+    Set<int> set1{-1, 0, 1, 2, 3};
+    Set<int> set2{2, 3, 4, 5};
+    Set<int> result{set::intersection(set1, set2)};
+    assert(result.items() == (std::vector<int>{2, 3}));
+}
+
+void test_set_difference()
+{
+    Set<int> set1{-1, 0, 1, 2, 3};
+    Set<int> set2{2, 3, 4, 5};
+
+    Set<int> diff1{set::difference(set1, set2)};
+    assert(diff1.items() == (std::vector<int>{-1, 0, 1}));
+
+    Set<int> diff2{set::difference(set2, set1)};
+    assert(diff2.items() == (std::vector<int>{4, 5}));
+}
+
 int main()
 {
+    test_set_add();
     test_set_contains();
     test_set_items();
     test_set_outstream();
     test_item_constructor();
     test_vector_constructor();
     test_hash_conflict();
+    test_set_union();
+    test_set_intersection();
+    test_set_difference();
 }
