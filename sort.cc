91580f02300bb3d046583284af4af2b22d742399
Liam Watts <liam.watts271@gmail.com>
2023-07-18 17:14:23 +0800

    sort: minor quicksort optimization

--- sort.cc
+++ sort.cc
@@ -31,7 +31,7 @@ void bubblesort(std::vector<int> &nums)
     } while (swaps > 0);
 }
 
-void _quicksort(std::vector<int> &nums, int low, int high)
+void _quicksort(std::vector<int> &nums, const int low, const int high)
 {
     // split into two lists: one with elems less than pivot + pivot, second with elems >= pivot
     // then quicksort those lists
@@ -46,19 +46,34 @@ void _quicksort(std::vector<int> &nums, int low, int high)
         // if less than pivot, shift pivot up one and put nums[i] in its place
         if (nums[i] < pivot)
         {
-            std::swap(nums[pivot_idx + 1], nums[i]);
+            if (i != pivot_idx + 1)
+                std::swap(nums[pivot_idx + 1], nums[i]);
             std::swap(nums[pivot_idx], nums[pivot_idx + 1]);
             pivot_idx++;
         }
     }
 
-    _quicksort(nums, low, pivot_idx + 1);
+    // sort below and above the pivot_idx, the pivot_idx itself is already in sorted position
+    // as it all smaller numbers are below it, and larger numbers above
+    _quicksort(nums, low, pivot_idx);
     _quicksort(nums, pivot_idx + 1, high);
 }
 
-void quicksort(std::vector<int> &nums)
+void quicksort(std::vector<int> &nums) { _quicksort(nums, 0, nums.size()); }
+
+void test_quicksort()
 {
-    _quicksort(nums, 0, nums.size());
+    std::vector<int> vect3{4, 3, 5, 4};
+    quicksort(vect3);
+    assert(vect3 == (std::vector<int>{3, 4, 4, 5}));
+
+    std::vector<int> vect4{10, -3, 100, 2, 0, 40, -1};
+    quicksort(vect4);
+    assert(vect4 == (std::vector<int>{-3, -1, 0, 2, 10, 40, 100}));
+
+    std::vector<int> vect5{4, 4};
+    quicksort(vect5);
+    assert(vect5 == (std::vector<int>{4, 4}));
 }
 
 int main()
@@ -66,16 +81,13 @@ int main()
     std::vector<int> vect{1, 2, 3, 4, 5};
     std::cout << to_str(is_sorted(vect)) << std::endl;
 
-    // swapping works
-    std::swap(vect[0], vect[1]);
-    std::cout << vect << std::endl;
-
     // check sorting
     std::vector<int> vect2{10, -5, 3, 4, 0};
     bubblesort(vect2);
     std::cout << vect2 << std::endl;
 
     // quicksort
+    test_quicksort();
     std::vector<int> vect3{4, 3, 5, 4};
     std::vector<int> vect4{10, -3, 100, 2, 0, 40, -1};
     std::vector<int> vect5{4, 4};
