54f9f87fba0deff481527693cd187ba152d1bd21
Liam Watts <liam.watts271@gmail.com>
2023-07-26 16:40:12 +0800

    bst: calculate tree height and size

--- bst.cc
+++ bst.cc
@@ -4,6 +4,9 @@
 #include <assert.h>
 #include <iterator>
 #include <ranges>
+#include <cmath>
+#include <functional>
+#include "functools.h"
 #include "itertools.h"
 #include "testlib.h"
 
@@ -45,10 +48,31 @@ public:
         return node_exists(find_node(item));
     }
 
+    // technically not preorder because it includes gaps
     std::vector<std::optional<T>> preorder_traversal() const
     {
         return items;
     }
+
+    std::vector<T> inorder_traversal() const
+    {
+        std::vector<T> result{};
+        inorder_traversal(0, result);
+        return result;
+    }
+
+    // O(1) calculation of height
+    // uses the recurrence relation that Ri = 2*R(i-1) + 2, where Ri is the Rightmost node of level i (root is at level 0)
+    // algebra gives Ri = 2^(i+1) - 2
+    // gives i = log2(Ri + 2) - 1
+    // so i = ceil(log2(index of last node in tree + 2)) - 1  , taking the ceiling since if even one node is in a level, the tree has that height
+    long height() const
+    {
+        return std::lround(std::ceil(std::log2(items.size() + 1))) - 1;
+    }
+
+    // O(n) calculation of size (the number of nodes in the tree)
+    long size() const { return inorder_traversal().size(); }
 
 private:
     // items[0]: root
@@ -57,6 +81,10 @@ private:
     // items[2*i + 1]: left node of i
     // items[2*i + 2]: right node of i
     std::vector<std::optional<T>> items;
+
+    int get_left(const int idx) const { return 2 * idx + 1; }
+
+    int get_right(const int idx) const { return 2 * idx + 2; }
 
     bool node_exists(const int idx) const
     {
@@ -72,11 +100,11 @@ private:
         {
             const T current_val{items[current_node].value()};
             if (item < current_val)
-                current_node = 2 * current_node + 1;
+                current_node = get_left(current_node);
             else if (item == current_val)
                 return current_node;
             else
-                current_node = 2 * current_node + 2;
+                current_node = get_right(current_node);
         }
         return current_node;
     }
@@ -87,6 +115,16 @@ private:
         while (idx >= items.size())
             items.push_back(std::nullopt);
         items[idx] = item;
+    }
+
+    void inorder_traversal(const int start_idx, std::vector<T> &output_vec) const
+    {
+        if (!node_exists(start_idx))
+            return;
+
+        inorder_traversal(get_left(start_idx), output_vec);
+        output_vec.push_back(items[start_idx].value());
+        inorder_traversal(get_right(start_idx), output_vec);
     }
 };
 
@@ -133,8 +171,43 @@ void test_bst_contains()
     assert(bst.contains(1));
 }
 
+void test_bst_inorder_traversal()
+{
+    BinaryTree<int> tree1{3, 0, 6, 5, 7, 8};
+    std::vector<int> result1{tree1.inorder_traversal()};
+    assert(result1 == (std::vector<int>{0, 3, 5, 6, 7, 8}));
+
+    BinaryTree<int> tree2{10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 11, 15, 13, 14};
+    std::vector<int> result2{tree2.inorder_traversal()};
+    assert(result2 == (std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15}));
+}
+
+void test_bst_height()
+{
+    // height of an empty tree is undefined I guess assert((BinaryTree<int>{}).height() == 0);
+    assert((BinaryTree{1}).height() == 0);
+    assert((BinaryTree{3, 0, 5}).height() == 1);
+    assert((BinaryTree{3, 0, 5, 6, 7, 8}).height() == 4);
+    BinaryTree tree4{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 11, 15, 13, 14};
+    long height{tree4.height()};
+    assert(height == 9);
+    assert((BinaryTree{10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 11, 15, 13, 14}).height() == 9);
+}
+
+void test_bst_size()
+{
+    assert((BinaryTree{1}).size() == 1);
+    assert((BinaryTree{3, 0, 5}).size() == 3);
+    assert((BinaryTree{3, 0, 5, 6, 7, 8}).size() == 6);
+    assert((BinaryTree{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 11, 15, 13, 14}).size() == 14);
+    assert((BinaryTree{10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 11, 15, 13, 14}).size() == 14);
+}
+
 int main()
 {
     test_bst_add();
     test_bst_outstream();
+    test_bst_inorder_traversal();
+    test_bst_height();
+    test_bst_size();
 }
